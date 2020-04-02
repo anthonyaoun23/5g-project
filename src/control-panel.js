a0faed52054be6c7cccba10d5474be6fc422b887
Anthony <anthony.aoun1@hotmail.com>
2020-04-02 13:24:33 -0400

    small tweaks

--- src/control-panel.js
+++ src/control-panel.js
@@ -5,12 +5,12 @@ export default class ControlPanel extends PureComponent {
   render() {
     return (
       <div className="control-panel">
-        <h3>5g Around The World</h3>
+        <h3>5G Around The World</h3>
         <p>
-          Map sharing news and government articles related to 5g in countries
+          Map sharing news and government articles related to 5G in countries
           around the world.
         </p>
-        <p>Project for CSI2911</p>
+        <p>Project for SEG2911</p>
       </div>
     );
   }
